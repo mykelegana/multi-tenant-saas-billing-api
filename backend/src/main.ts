f09a3690a53b0f935a5730a770ae5017267891e8
Mykeleganerz <eganajustin@gmail.com>
2026-07-20 23:51:05 +0800

    added swagger for api docs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Multi-Tenant-Saas-Billing API')
+    .setDescription("This is the documentation of Multi-Tenant-Saas-Billing's API")
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
