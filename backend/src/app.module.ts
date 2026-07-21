4291c58dc9c88988d0fb30f5c0c6357d18bb2cd7
Mykeleganerz <eganajustin@gmail.com>
2026-07-22 00:17:57 +0800

    added users, organizations modules in app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,11 @@ import { AuthModule } from './modules/auth/auth.module';
 import { DatabaseService } from './database/database.service';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
+import { OrganizationsModule } from './modules/organizations/organizations.module';
 
 @Module({
-  imports: [AuthModule, DatabaseModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [DatabaseModule, AuthModule, UsersModule, OrganizationsModule ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService, DatabaseService],
 })
