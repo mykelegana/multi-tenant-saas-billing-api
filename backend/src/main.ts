97d758a51d51fde0acc265ae0b5bbf042d00f321
Mykeleganerz <eganajustin@gmail.com>
2026-07-26 23:57:40 +0800

    added bearer auth in swagger ui

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
     .setTitle('Multi-Tenant-Saas-Billing API')
     .setDescription("This is the documentation of Multi-Tenant-Saas-Billing's API")
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
