3fcd03b31d5d85cd14dffd5d041191fc1baef583
Mykeleganerz <eganajustin@gmail.com>
2026-08-07 15:59:14 +0800

    feat(stripe-billing) initialized stripe client and applied correct file structure

--- backend/src/modules/stripe/stripe.module.ts
+++ backend/src/modules/stripe/stripe.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
+import Stripe from 'stripe'
 import { StripeService } from './stripe.service';
-import { StripeController } from './stripe.controller';
 
+@Global()
 @Module({
-  controllers: [StripeController],
-  providers: [StripeService],
+  providers: [
+    {
+      provide: 'STRIPE_CLIENT',
+      useFactory: () => {
+        return new Stripe(process.env.STRIPE_PRIVATE_KEY!);
+      },
+    },
+    StripeService
+  ],
+  exports: ['STRIPE_CLIENT', StripeService],
 })
-export class StripeModule {}
+export class StripeModule { }
--- backend/src/modules/stripe/stripe.service.ts
+++ backend/src/modules/stripe/stripe.service.ts
@@ -1,26 +1,7 @@
-import { Injectable } from '@nestjs/common';
-import { CreateStripeDto } from './dto/create-stripe.dto';
-import { UpdateStripeDto } from './dto/update-stripe.dto';
+import { Inject, Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import Stripe from "stripe";
 
 @Injectable()
 export class StripeService {
-  create(createStripeDto: CreateStripeDto) {
-    return 'This action adds a new stripe';
-  }
-
-  findAll() {
-    return `This action returns all stripe`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} stripe`;
-  }
-
-  update(id: number, updateStripeDto: UpdateStripeDto) {
-    return `This action updates a #${id} stripe`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} stripe`;
-  }
+    constructor(@Inject('STRIPE_CLIENT') stripe: Stripe) { }
 }
