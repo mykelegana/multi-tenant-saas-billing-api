a6eb1b55dbf85dab6d38d2e90c13196485f13444
Mykeleganerz <eganajustin@gmail.com>
2026-08-20 22:45:01 +0800

    feat(rate-limit): imported usage service, initial rate limit service

--- backend/src/modules/rate-limit/rate-limit.module.ts
+++ backend/src/modules/rate-limit/rate-limit.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RateLimitService } from './rate-limit.service';
+import { RateLimitGuard } from './rate-limit.guard';
+import { UsageService } from '../usage/usage.service';
+import { UsageModule } from '../usage/usage.module';
 
 @Module({
-  providers: [RateLimitService]
+  providers: [RateLimitService, RateLimitGuard],
+  imports: [UsageModule]
 })
-export class RateLimitModule {}
+export class RateLimitModule { }
--- backend/src/modules/rate-limit/rate-limit.service.ts
+++ backend/src/modules/rate-limit/rate-limit.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { UsageService } from '../usage/usage.service';
+import { Plan } from '@prisma/client';
+import Redis from 'ioredis';
 
 @Injectable()
-export class RateLimitService {}
+export class RateLimitService {
+    constructor(private readonly usageService: UsageService, @Inject('REDIS_CLIENT') private redis: Redis) { }
+
+    async checkLimit(orgId: string, plan: Plan) {
+        const currentMinute = Math.floor(Date.now() / 60000);
+
+        const key = `rate-limit:${orgId}:${currentMinute}`;
+
+        const currentCount = await this.redis.get(key);
+
+        const count = Number(currentCount ?? 0);
+
+    }
+
+}
