adc6ad4421187b4bf7a30132fca77bac837692ef
Mykeleganerz <eganajustin@gmail.com>
2026-08-21 23:57:19 +0800

    feat(rate-limit): initialized rate limit guard

--- backend/src/modules/rate-limit/rate-limit.guard.ts
+++ backend/src/modules/rate-limit/rate-limit.guard.ts
@@ -1,5 +1,10 @@
-import { Injectable } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Observable } from "rxjs";
 
 
 @Injectable()
-export class RateLimitGuard { }
+export class RateLimitGuard implements CanActivate {
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+
+    }
+}
